27113d3533df6350dcd952b28e05d523f999bb93
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-04 16:42:22 +0300

    Создание шаблона

--- routes/index.js
+++ routes/index.js
@@ -8,26 +8,47 @@ router.get('/', function(req, res, next) {
 
 /* Страница Клариссы */
 router.get('/clary', function(req, res, next) {
-    res.send("<h1>Страница Клариссы</h1>")
+    res.render('hunter', {
+        title: "Кларисса Фейрчайлд",
+        picture: "images/clary.webp",
+        desc: "Сумеречная охотница. Воссоздает мощные древние руны, не написанные в Серой книге."
+    });
+
 });
 /* Страница Джейса */
 router.get('/jace', function(req, res, next) {
-    res.send("<h1>Страница Джейса</h1>")
+    res.render('hunter', {
+        title: "Джейс Эрондейл",
+        picture: "images/jace.webp",
+        desc: "Со-глава Института Нью-Йорка. Может активировать руны без стеле."
+    });
 });
 
 /* Страница Алека */
 router.get('/alec', function(req, res, next) {
-    res.send("<h1>Страница Алека</h1>")
+    res.render('hunter', {
+        title: "Александр Лайтвуд",
+        picture: "images/alec.webp",
+        desc: "Консул Конклава-в-изгнании."
+    });
 });
 
 /* Страница Магнуса */
 router.get('/magnus', function(req, res, next) {
-    res.send("<h1>Страница Магнуса</h1>")
+    res.render('hunter', {
+        title: "Магнус Бейн",
+        picture: "images/magnus.webp",
+        desc: "Верховный маг Бруклина."
+    });
 });
 
 /* Страница Изабель */
 router.get('/isabel', function(req, res, next) {
-    res.send("<h1>Страница Изабель</h1>")
+    res.render('hunter', {
+        title: "Изабель Лайтвуд",
+        picture: "images/isabel.webp",
+        desc: "Сумеречная охотница. Обладательница мощного амулета, который обнаруживает демонов."
+    });
 });
 
 
