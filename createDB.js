efcb4350fa61ddc6198b6227c1318e6bdb231af1
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-04 18:29:45 +0300

    Посев данных приложения

--- createDB.js
+++ createDB.js
@@ -1,6 +1,5 @@
 var MongoClient = require('mongodb').MongoClient
 var data = require("./data.js").data
-
 const uri = "mongodb://localhost:27017/"
 const client = new MongoClient(uri)
 async function run() {
@@ -10,8 +9,11 @@ var database = client.db("hunters");
 database.dropDatabase()
 database = client.db("hunters");
 const hunter = database.collection("hunter");
-const result = await hunter.insertOne({name:"Alec"});
-console.log(`${result} documents were inserted`);
+const result = await hunter.insertMany(data);
+console.log(`${result.insertedCount} documents were inserted`);
+for (const key in result) {
+            console.log(`${key}: ${result[key]}`);
+            }
 } finally {
 await client.close();
 }
