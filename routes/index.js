393295849831cd71ef3d92677ac58c81bc3c6b59
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-05 15:54:01 +0300

    Сохранение session в mongodb

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,9 @@ var Hunter = require("../models/hunter").Hunter
 /* GET home page. */
 router.get('/', function(req, res, next) {
 Hunter.find({},{_id:0,title:1,nick:1},function(err,menu){
-    res.cookie('greeting', 'HI!!').render('index', { title: 'Сумеречные охотники', menu: menu});
-    })
+    req.session.greeting = "Hi!!!"
+    res.render('index', { title: 'Сумеречные охотники', menu: menu });
+});
 });
 
 // /* Страница Клариссы */
