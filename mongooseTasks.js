a596cc7c83bdc2883342a04c43cbe3bf675b1e42
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-05 13:13:19 +0300

    Создание модели данных приложения

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,15 +1,24 @@
 var mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost/test1')
+var Hunter = require("./models/hunter").Hunter
 
-
-var schema = mongoose.Schema({ name: String })
-schema.methods.hey = function(){
-    console.log(this.get("name") + " said hey")
-}
-var Hunter = mongoose.model('Hunter', schema)
-
-var hunt = new Hunter({ name: 'Alec' })
-hunt.save(function (err) {
-    hunt.hey()
+var hunter = new Hunter({
+    title: "Alec",
+    nick: "alec"
 })
+console.log(hunter)
+hunter.save(function(err, hunter, affected){
+    console.log(hunter.title)
+})
+
+// var schema = mongoose.Schema({ name: String })
+// schema.methods.hey = function(){
+//     console.log(this.get("name") + " said hey")
+// }
+// var Hunter = mongoose.model('Hunter', schema)
+
+// var hunt = new Hunter({ name: 'Alec' })
+// hunt.save(function (err) {
+//     hunt.hey()
+// })
