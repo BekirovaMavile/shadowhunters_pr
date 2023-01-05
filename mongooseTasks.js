6299cdcd5620b426eca5306d26d566f8bd8325f0
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-05 13:03:15 +0300

    Создание схемы

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -3,14 +3,13 @@ mongoose.set('strictQuery', true);
 mongoose.connect('mongodb://localhost/test1')
 
 
-
-var Hunter = mongoose.model('Hunter', { name: String })
+var schema = mongoose.Schema({ name: String })
+schema.methods.hey = function(){
+    console.log(this.get("name") + " said hey")
+}
+var Hunter = mongoose.model('Hunter', schema)
 
 var hunt = new Hunter({ name: 'Alec' })
 hunt.save(function (err) {
-    if (err) {
-        console.log(err)
-    } else {
-        console.log('hey!')
-    }
+    hunt.hey()
 })
