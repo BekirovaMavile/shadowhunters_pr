e3746d425fa4d853f5c377c2f3ba1b7fc059ebf7
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-06 17:11:04 +0300

    Заполнение меню из базы

--- middleware/createMenu.js
+++ middleware/createMenu.js
@@ -1,13 +1,23 @@
-var Hunter = require("../models/hunter").Hunter
-
+var db = require('../mySQLConnect.js');
 
 module.exports = function(req,res,next){
-    res.locals.nav = []
-
-    Hunter.find(null,{_id:0,title:1,nick:1},function(err,result){
+res.locals.nav = []
+    db.query(`SELECT * FROM hunter`, (err, hunters) => {  
         if(err) throw err
-        res.locals.nav = result
+        res.locals.nav = hunters
         next()
+        
     })
-
 }
+// var Hunter = require("../models/hunter").Hunter
+
+// module.exports = function(req,res,next){
+//     res.locals.nav = []
+
+//     Hunter.find(null,{_id:0,title:1,nick:1},function(err,result){
+//         if(err) throw err
+//         res.locals.nav = result
+//         next()
+//     })
+
+// }
