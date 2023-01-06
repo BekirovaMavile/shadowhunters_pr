562b44e8152394599f22d2103319b3a29f2753e7
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-06 16:50:31 +0300

    Вывод данных на экран

--- routes/hunters.js
+++ routes/hunters.js
@@ -1,47 +1,40 @@
 var express = require('express');
 var router = express.Router();
-var Hunter = require("../models/hunter").Hunter
-var async = require("async")
-var checkAuth = require("../middleware/checkAuth.js")
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('Новый маршрутизатор, для маршрутов, начинающихся с hunters');
-// });
+var db = require('../mySQLConnect.js');
+// var Hunter = require("../models/hunter").Hunter
+// var checkAuth = require("../middleware/checkAuth.js")
+
+/* GET hunters listing. */
+router.get('/', function(req, res, next) {
+res.send('<h1>Это экран для списка охотников</h1>');
+});
+
 
 /* Страница охотников */
-router.get("/:nick", checkAuth, function(req, res, next){
-    // async.parallel([
-    //     function(callback){
-    //         Hunter.findOne({nick: req.params.nick},callback)
-    //     },
-    //     function(callback){
-    //         Hunter.find({},{_id:0,title:1,nick:1}, callback)
-    //     }
-    // ],
-    // function(err, result){
+router.get("/:nick", function(req, res, next){
+    db.query(`SELECT * FROM hunter WHERE hunter.nick = '${req.params.nick}'`, (err, hunters) => {             
+        if(err) {
+            console.log(err);
+            if(err) return next(err)
+        } else {
+            if(hunters.length == 0) return next(new Error("Нет такого охотника в книге"))
+            var hunter = hunters[0];
+            res.render('hunter', {
+            title: hunter.title,
+            picture: hunter.avatar,
+            about: hunter.about
+        })
+        }
+    })
+    // Hunter.findOne({nick: req.params.nick}, function(err, hunter){
     //     if(err) return next(err)
-    //     var hunter = result[0]
-    //     var hunters = result[1] || []
-    //     // console.log(hunters)
     //     if(!hunter) return next (new Error("Нет такого охотника в этой книге"))
-    //     // console.log(hunter.avatar)
     //     res.render('hunter', {
     //         title: hunter.title,
     //         picture: hunter.avatar,
-    //         desc: hunter.desc, 
-    //         menu: hunters
+    //         desc: hunter.desc
     //     })
     // })
-    Hunter.findOne({nick: req.params.nick}, function(err, hunter){
-        if(err) return next(err)
-        // console.log(hunter)
-        if(!hunter) return next (new Error("Нет такого охотника в этой книге"))
-        res.render('hunter', {
-            title: hunter.title,
-            picture: hunter.avatar,
-            desc: hunter.desc
-        })
-    })
 });
 
 module.exports = router;
